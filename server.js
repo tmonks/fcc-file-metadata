894aabcfbfb6931ec069d521892ba719b8c00e9e
Glitch (tmonks-file-metadata) <none>
2019-12-05 19:19:32 +0000

    🏩🦂 Updated with Glitch

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -14,8 +14,13 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+// FILE UPLOADS
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if(req.file) {
+    res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+  } else {
+    res.json({error: "No file uploaded"});
+  }
 });
 
 app.listen(process.env.PORT || 3000, function () {
